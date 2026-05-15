03c2be797bdb15ceb3acf9a302f25ad73bf790de
jhoyossalgado <83507787+jhoyossalgado@users.noreply.github.com>
2026-05-14 21:11:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import numpy as np
 #from flask import Flask, request, jsonify, render_template, url_for
 import pickle
-from sklearn import svm
+#from sklearn import svm
 import streamlit as st
 #from tensorflow.keras.models import load_model
 
