81a5d407d03f2f374387415b88522a495acfeee5
jhoyossalgado <83507787+jhoyossalgado@users.noreply.github.com>
2026-05-14 21:39:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import streamlit as st
 #model = load_model("modelo.h5")
 
 # Path del modelo preentrenado
-MODEL_PATH = 'modelodesnInfANN.pkl'
+MODEL_PATH = 'modelodesnInfSVC.pkl'
 
 
 # Se recibe la imagen y el modelo, devuelve la predicción
