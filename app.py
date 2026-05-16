f3d3b4df216149e4c3ce148188cef3f6985b77c6
jhoyossalgado <83507787+jhoyossalgado@users.noreply.github.com>
2026-05-16 15:58:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import numpy as np
 ##from sklearn.externals import joblib
 import streamlit as st
 import pickle
-import sciklearn
+import sklearn
 #from sklearn import svm
 import streamlit as st
 #from tensorflow.keras.models import load_model
